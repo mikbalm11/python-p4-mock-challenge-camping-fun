ae2fcc1b78597b4a38d37f54fc5526889aa39c34
Mustafa Mehmedoglu <mikbalm@gmail.com>
2025-05-13 23:25:34 -0400

    challenge completed!

--- server/app.py
+++ server/app.py
@@ -20,10 +20,162 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
+api = Api(app)
 
 @app.route('/')
 def home():
     return ''
+
+class Campers(Resource):
+    def get(self):
+        campers = [camper.to_dict(rules=('-signups',)) for camper in Camper.query.all()]
+
+        result = make_response(
+            campers,
+            200
+        )
+        return result
+
+    def post(self):
+        try:
+            new_camper = Camper(
+                name=request.json['name'],
+                age=request.json['age']
+            )
+
+            db.session.add(new_camper)
+            db.session.commit()
+
+            result = make_response(
+                new_camper.to_dict(rules=('-signups',)),
+                201
+            )
+
+            return result
+        except ValueError:
+            result = make_response(
+                {"errors": ["validation errors"]},
+                400
+            )
+
+            return result
+
+api.add_resource(Campers, "/campers")
+
+class CampersById(Resource):
+    def get(self, id):
+        camper = Camper.query.filter(Camper.id == id).first()
+
+        if camper is None:
+            result = make_response(
+                {'error': 'Camper not found'},
+                404
+            )
+            return result
+
+        result = make_response(
+            camper.to_dict(),
+            200
+        )
+
+        return result 
+
+    def patch(self, id):
+        camper = Camper.query.filter(Camper.id == id).first()
+
+        if camper is None:
+            result = make_response(
+                {'error': 'Camper not found'},
+                404
+            )
+            return result
+
+        fields = request.get_json()
+
+        try:
+            setattr(camper, 'name', fields['name'])
+            setattr(camper, 'age', fields['age'])
+            db.session.add(camper)
+            db.session.commit()
+
+            result = make_response(
+                camper.to_dict(rules=('-signups',)),
+                202
+            )
+            return result
+
+        except ValueError:
+            result = make_response(
+                {"errors": ["validation errors"]},
+                400
+            )
+
+            return result
+
+api.add_resource(CampersById, "/campers/<int:id>")
+
+class Activities(Resource):
+    def get(self):
+        activities = [activity.to_dict(rules=('-signups',)) for activity in Activity.query.all()]
+
+        result = make_response(
+            activities,
+            200
+        )
+        return result
+
+api.add_resource(Activities, "/activities")
+
+class ActivityById(Resource):
+    def delete(self, id):
+        activity = Activity.query.filter(Activity.id == id).first()
+
+        if activity is None:
+            result = make_response(
+                {'error': 'Activity not found'},
+                404
+            )
+            return result
+
+        db.session.delete(activity)
+        db.session.commit()
+
+        result = make_response(
+            '',
+            204
+        )
+
+        return result
+
+api.add_resource(ActivityById, "/activities/<int:id>")
+
+class Signups(Resource):
+    def post(self):
+        try:
+            new_signup = Signup(
+                time=request.json["time"],
+                camper_id=request.json["camper_id"],
+                activity_id=request.json["activity_id"]
+            )
+
+            db.session.add(new_signup)
+            db.session.commit()
+
+            result = make_response(
+                new_signup.to_dict(),
+                201
+            )
+
+            return result
+
+        except ValueError:
+            result = make_response(
+                {"errors": ["validation errors"]},
+                400
+            )
+            return result
+
+api.add_resource(Signups, "/signups")
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -25,8 +25,10 @@ class Activity(db.Model, SerializerMixin):
     difficulty = db.Column(db.Integer)
 
     # Add relationship
-    
+    signups = db.relationship('Signup', backref='activity')
+
     # Add serialization rules
+    serialize_rules = ('-signups.activity',)
     
     def __repr__(self):
         return f'<Activity {self.id}: {self.name}>'
@@ -40,10 +42,23 @@ class Camper(db.Model, SerializerMixin):
     age = db.Column(db.Integer)
 
     # Add relationship
+    signups = db.relationship('Signup', backref='camper')
     
     # Add serialization rules
+    serialize_rules = ('-signups.camper',)
     
     # Add validation
+    @validates('name')
+    def validate_name(self, key, name):
+        if not name:
+            raise ValueError("Campers must have names.")
+        return name
+    
+    @validates('age')
+    def validate_age(self, key, age):
+        if age < 8 or age > 18:
+            raise ValueError("Campers must be between ages 8 to 18.")
+        return age
     
     
     def __repr__(self):
@@ -56,11 +71,19 @@ class Signup(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     time = db.Column(db.Integer)
 
-    # Add relationships
-    
+    # Add relationship
+    activity_id = db.Column(db.Integer, db.ForeignKey('activities.id'))
+    camper_id = db.Column(db.Integer, db.ForeignKey('campers.id'))
+
     # Add serialization rules
-    
+    serialize_rules = ('-activity.signups', '-camper.signups')
+
     # Add validation
+    @validates('time')
+    def validate_time(self, key, time):
+        if time < 0 or time > 23:
+            raise ValueError("Time of day must be between 0 to 23")
+        return time
     
     def __repr__(self):
         return f'<Signup {self.id}>'
